be7889f93c7cb6c0a6ccf1e65e05eb0d71c85386
Bojan Devic <bojan@bojandevic.com>
2016-06-03 16:32:09 +0200

    added logging of all commands to ~/.logs folder

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Coping dot files"
 
+if [[ !  -d ~/.logs ]]; then
+   echo "Making ~/.logs folder"
+   mkdir ~/.logs
+fi
+
 SCRIPTS=(".bashrc" ".aliases" ".vimrc" ".functions" ".jshintrc")
 for SCRIPT in ${SCRIPTS[@]} 
 do
