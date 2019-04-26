7e0032378671026e511bfa2844782028b2693b26
Bojan Devic <bojan@bojandevic.com>
2019-04-26 20:44:52 +0200

    added vscodes settings.json to backup

--- collect.sh
+++ collect.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 echo "Coping dot files"
 
-SCRIPTS=(".bashrc" ".aliases" ".vimrc" ".functions" ".jshintrc" ".gitconfig")
-for SCRIPT in ${SCRIPTS[@]} 
+FILES=(".bashrc" ".aliases" ".vimrc" ".functions" ".jshintrc" ".gitconfig")
+for FILE in ${FILES[@]}
 do
-    cp ~/$SCRIPT $SCRIPT
+    cp ~/$FILE $FILE
 done
 
-echo "Coping Sublime 3 files"
-cp -r ~/.config/sublime-text-3/Installed\ Packages Sublime\ 3/
-cp -r ~/.config/sublime-text-3/Packages Sublime\ 3/
+SUBLIMEDIR=~/.config/sublime-text-3
+if [ -d "$SUBLIMEDIR" ]; then
+    echo "Coping Sublime 3 files"
+    cp -r $SUBLIMEDIR/Installed\ Packages Sublime\ 3/
+    cp -r $SUBLIMEDIR/Packages Sublime\ 3/
+fi
+
+VSCODEDIR=~/.config/Code/User
+if [ ! -d "$VSCODEDIR" ]; then
+echo "change"
+    VSCODEDIR=~/.config/Code\ -\ OSS/User
+fi
+
+if [ -d "$VSCODEDIR" ]; then
+    echo "Coping VS Code files"
+    mkdir -p vscode
+    cp -r "$VSCODEDIR/settings.json" vscode
+fi
